e4a61a27022d2a6929bb89beb015eb74574e8c36
+ <+>
2023-08-04 16:16:26 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, search_string) {
+    search_string = search_string.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => 
+        driver.toLowerCase().includes(search_string));
+        return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, match_string) {
+    match_string = match_string.toLowerCase();
+    const sameLetters = drivers.filter(driver => 
+        driver.toLowerCase().startsWith(match_string));
+         return sameLetters;
+}
+
+function matchName(drivers, match_string) {
+  const matchingDrivers = [];
+ 
+  for (let i = 0; i < drivers.length; i++) {
+    let currentDriver = drivers[i];
+
+    if(currentDriver.name === match_string) {
+        matchingDrivers.push(currentDriver);
+    }
+  }
+  return matchingDrivers;
+}
